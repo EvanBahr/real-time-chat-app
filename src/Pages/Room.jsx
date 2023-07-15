19a6f95e8f93df2d04d2578a44d0a329edbdf762
EvanBahr <evanbahrdev@gmail.com>
2023-07-15 10:10:02 +0700

    add create and delete message

--- src/Pages/Room.jsx
+++ src/Pages/Room.jsx
@@ -4,23 +4,64 @@ import {
   DATABASE_ID,
   databases,
 } from "../appwriteConfig";
+import { ID, Query } from "appwrite";
 const Room = () => {
   const [Messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState("");
+  console.log("new message", messageBody);
   useEffect(() => {
     getMessages();
   }, []);
   const getMessages = async () => {
     const response = await databases.listDocuments(
       DATABASE_ID,
-      COLLECTION_ID_MESSAGES
+      COLLECTION_ID_MESSAGES,
+      [Query.orderDesc("$createdAt"), Query.limit(5)]
     );
-
     console.log("response", response.documents);
     setMessages(response.documents);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let payload = {
+      body: messageBody,
+    };
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    );
+    setMessageBody("");
+    console.log("created", response);
+    getMessages((prevState) => [response, ...Messages]);
+    console.log("updated", Messages);
+  };
+
+  const DeleteMessage = async (message_id) => {
+    databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, message_id);
+    setMessages((prevState) =>
+      Messages.filter((message) => message.$id !== message_id)
+    );
+    // getMessages();
   };
   return (
     <main className="container">
       <div className="room--container">
+        <form onSubmit={handleSubmit} id="message--form">
+          <div>
+            <textarea
+              required
+              maxLength="1000"
+              placeholder="say something..."
+              onChange={(e) => setMessageBody(e.target.value)}
+              value={messageBody}
+            ></textarea>
+          </div>
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="Send" />
+          </div>
+        </form>
         <div>
           {Messages.map((message) => (
             <div key={message.$id} className="message--wrapper">
@@ -28,6 +69,13 @@ const Room = () => {
                 <small className="message-timestamp">
                   {message.$createdAt}
                 </small>
+                <button
+                  onClick={() => {
+                    DeleteMessage(message.$id);
+                  }}
+                >
+                  X
+                </button>
               </div>
               <div className="message--body">
                 <span>{message.body} </span>
--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,9 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, ID } from "appwrite";
 const client = new Client();
 export const PROJECT_ID = "64b0efd0586cb02d6f7a";
 export const DATABASE_ID = "64b0f2f419b09268e2e3";
 export const COLLECTION_ID_MESSAGES = "64b0f32f5e7fb6b4b014";
-
+export const DOCUMENT_ID = ID.unique();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("64b0efd0586cb02d6f7a");
