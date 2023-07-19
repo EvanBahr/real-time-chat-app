879274fff2b98c06d4021663a6c250765416b06f
EvanBahr <evanbahrdev@gmail.com>
2023-07-19 10:43:24 +0700

    membuat auth context , agak bingung tapi lumayan

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Room from "./Pages/Room";
 import LoginPage from "./Pages/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoutes from "./components/PrivateRoutes";
+import { AuthProvider } from "./utils/AuthContext";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/" element={<Room />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<Room />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
