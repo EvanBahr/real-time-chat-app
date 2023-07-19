36809f0215fec60fd62d70fc8cd1d1a983f31aba
EvanBahr <evanbahrdev@gmail.com>
2023-07-19 14:29:29 +0700

    add login user fitur

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,4 +1,4 @@
-import { Client, Databases, ID } from "appwrite";
+import { Account, Client, Databases, ID } from "appwrite";
 const client = new Client();
 export const PROJECT_ID = "64b0efd0586cb02d6f7a";
 export const DATABASE_ID = "64b0f2f419b09268e2e3";
@@ -8,4 +8,5 @@ client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("64b0efd0586cb02d6f7a");
 export const databases = new Databases(client);
+export const account = new Account(client);
 export default client;
--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,23 +1,52 @@
 import { useContext, useState, useEffect, createContext } from "react";
+import { account } from "../appwriteConfig";
+import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 
 //
 export const AuthProvider = ({ children }) => {
   const [Loading, setLoading] = useState(false);
-  const [user, setUser] = useState(true);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    getUserOnLoad();
+  }, []);
+  const handleUserLogin = async (e, Credentials) => {
+    e.preventDefault();
+    try {
+      const response = await account.createEmailSession(
+        Credentials.email,
+        Credentials.password
+      );
+      console.log("logged", response);
+      const accountDetail = account.get();
+      setUser(accountDetail);
+      navigate("/");
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
   const ContextData = {
     user,
+    handleUserLogin,
   };
-  useEffect(() => {
-    setLoading(false);
-  });
 
+  const getUserOnLoad = async () => {
+    try {
+      const accountDetail = account.get();
+      setUser(accountDetail);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
   return (
     <AuthContext.Provider value={ContextData}>
       {Loading ? <p>Loading...</p> : children}
     </AuthContext.Provider>
   );
 };
+
 export const useAuth = () => {
   return useContext(AuthContext);
 };
